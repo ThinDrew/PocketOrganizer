fc03211a469bae59895b3e46d50194013f226628
ThinDrew <andreynikonorov5@gmail.com>
2024-05-12 01:29:46 +0700

    Добавление перечислений для названий месяцев и дней недели

---
+++ app/src/main/java/com/example/pocketorganizer/DayOfWeekName.java
@@ -0,0 +1,11 @@
+package com.example.pocketorganizer;
+
+public enum DayOfWeekName {
+    Понедельник,
+    Вторник,
+    Среда,
+    Четверг,
+    Пятница,
+    Суббота,
+    Воскресенье
+}
---
+++ app/src/main/java/com/example/pocketorganizer/MonthName.java
@@ -0,0 +1,16 @@
+package com.example.pocketorganizer;
+
+public enum MonthName {
+    Январь,
+    Февраль,
+    Март,
+    Апрель,
+    Май,
+    Июнь,
+    Июль,
+    Август,
+    Сентябрь,
+    Октябрь,
+    Ноябрь,
+    Декабрь
+}
